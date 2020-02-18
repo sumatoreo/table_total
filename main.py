dceae5881f5889a8ea5d28682464537fe2e57953
Iskander Kamaliev <iskander.kamaliev@gmail.com>
2020-02-18 21:37:50 +0300

    User Agent

--- main.py
+++ main.py
@@ -17,8 +17,38 @@ HOST = 0
 PORT = 1
 PROTOCOL = 2
 
+user_agent_list = [
+    # Chrome
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    # Firefox
+    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'
+]
+
 
 def d(proxy, logger):
+    user_agent = random.choice(user_agent_list)
+
     with requests.Session() as s:
         s.proxies = {proxy[PROTOCOL]: '{protocol}://{host}:{port}'.format(
             protocol=proxy[PROTOCOL],
@@ -26,6 +56,8 @@ def d(proxy, logger):
             port=proxy[PORT]
         )}
         s.auth = HTTPBasicAuth(config.get('d', 'basic_username'), config.get('d', 'basic_password'))
+        s.headers = {'User-Agent': user_agent}
+
         host = config.get('d', 'host')
 
         r = s.post('https://{}/index.php'.format(host), data={
@@ -50,12 +82,15 @@ def d(proxy, logger):
 
 
 def p(proxy, logger):
+    user_agent = random.choice(user_agent_list)
+
     with requests.Session() as s:
         s.proxies = {proxy[PROTOCOL]: '{protocol}://{host}:{port}'.format(
             protocol=proxy[PROTOCOL],
             host=proxy[HOST],
             port=proxy[PORT]
         )}
+        s.headers = {'User-Agent': user_agent}
         host = config.get('p', 'host')
 
         r = s.post('https://{}/index.php'.format(host), data={
@@ -127,4 +162,5 @@ if __name__ == '__main__':
         break
 
     if try_count == 0:
-        logger.error('The number of attempts has been exhausted. Change the list of proxy servers or check your Internet connection')
+        logger.error(
+            'The number of attempts has been exhausted. Change the list of proxy servers or check your Internet connection')
